3683c6bf5b9f099c7df17c19974bfb40bf8a04a8
Iwan Morgan <ibmorgan0@gmail.com>
2017-10-08 10:42:31 +0100

    Added ProductController

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+        
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,10 +3,7 @@ using Ninject;
 using SportsStore.Domain.Abstract;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
