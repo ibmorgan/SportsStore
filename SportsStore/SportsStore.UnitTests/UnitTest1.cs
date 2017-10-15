c227c9d1c1be4a1ccf1f5461cb0f29e1075b1537
Iwan Morgan <ibmorgan0@gmail.com>
2017-10-15 22:43:34 +0100

    Added unitTest for page link creation.

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -6,6 +6,9 @@ using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
 
 namespace SportsStore.UnitTests
 {
@@ -40,9 +43,31 @@ namespace SportsStore.UnitTests
         }
 
         [TestMethod]
-        public void Can_Pagination_Work()
+        public void Can_Generate_Page_Links()
         {
+            // Arrange - define an HTML helper - we need to do this
+            // in order to apply the extension method
+            HtmlHelper myHelper = null;
 
+            // Arrange - create PagingInfo data
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - set up the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act 
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>" 
+                            + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>" 
+                            + @"<a class=""btn btn-default"" href=""Page3"">3</a>", 
+                            result.ToString());
         }
     }
 }
