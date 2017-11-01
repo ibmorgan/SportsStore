2b41df01b551d1c4bdc7578f72b8edad7e0f9140
Iwan Morgan <ibmorgan0@gmail.com>
2017-11-01 21:47:53 +0000

    Added NavController, Views and Unit test for better navigation.

---
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,27 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                                            .Select(p => p.Category)
+                                            .Distinct()
+                                            .OrderBy(p => p);
+
+            return PartialView(categories);
+        }
+    }
+}
