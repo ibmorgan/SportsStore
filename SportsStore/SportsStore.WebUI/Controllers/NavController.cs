865adb5c786023f6ebc5b95bba8f2d09631109c6
Iwan Morgan <ibmorgan0@gmail.com>
2017-11-02 18:18:44 +0000

    Added NavController and Menu with unit test.

--- SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -14,8 +14,10 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu()
+        public PartialViewResult Menu(string category = null)
         {
+            ViewBag.SelectedCategory = category;
+
             IEnumerable<string> categories = repository.Products
                                             .Select(p => p.Category)
                                             .Distinct()
