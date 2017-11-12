314ef65560a79d5947e385a3ecebd2173f1324c2
Iwan Morgan <ibmorgan0@gmail.com>
2017-11-12 20:58:31 +0000

    Added EmailOrderProcessor and ShippingDetails class. Updated Index and checkout views.

--- SportsStore/SportsStore.Domain/Entities/ShippingDetails.cs
+++ SportsStore/SportsStore.Domain/Entities/ShippingDetails.cs
@@ -8,8 +8,11 @@ namespace SportsStore.Domain.Entities
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter the first address line")]
+        [Display(Name="Line 1")]
         public string Line1 { get; set; }
+        [Display(Name="Line 2")]
         public string Line2 { get; set; }
+        [Display(Name="Line 3")]
         public string Line3 { get; set; }
 
         [Required(ErrorMessage = "Please enter a city name")]
@@ -18,6 +21,7 @@ namespace SportsStore.Domain.Entities
         [Required(ErrorMessage = "Please enter a county name")]
         public string County { get; set; }
 
+        [Display(Name="Post Code")]
         public string PostCode { get; set; }
 
         [Required(ErrorMessage = "Please enter a country name")]
--- SportsStore/SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -57,5 +57,10 @@ namespace SportsStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
