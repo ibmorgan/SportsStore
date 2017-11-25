4110b6d720c94a435d9db216a3e326ef672cf5dc
Iwan Bowen Morgan <ibmorgan0@gmail.com>
2017-11-25 10:13:54 +0000

    Built out the product edit features in the admin section. Updated code to allow product to be saved.

--- SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -15,6 +17,29 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Index()
         {
             return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productID)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productID);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(product);
+            }
         }
     }
 }
