979da8a2fe8eb21f89dfe477bcd13b879ce5e01d
Iwan Morgan <ibmorgan0@gmail.com>
2017-11-30 20:27:06 +0000

    Added unit tests for AdminSecurity

---
+++ SportsStore/SportsStore.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            // Arrange - create a mock authentication provider
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+
+            // Arrange - create the view model
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "admin",
+                Password = "secret"
+            };
+
+            // Arrange - create the controller
+            AccountController target = new AccountController();
+        }
+    }
+}
